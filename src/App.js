92e3c37bbfb37ac3516468da70c9ba7ae8171508
Terry Threatt <terry.threatt@gmail.com>
2019-05-06 14:33:00 -0500

    created question component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import Questions from './components/Questions.js';
+import Question from './components/Question/Question';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <p>Work in progress.</p>
-        <Questions />
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
       </div>
     );
   }
