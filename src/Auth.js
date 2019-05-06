ca88b9b9c52c81ac83ad64657b4eec1dfe24182e
Terry Threatt <terry.threatt@gmail.com>
2019-05-06 15:14:33 -0500

    setup auth0 authentication

---
+++ src/Auth.js
@@ -0,0 +1,63 @@
+import auth0 from 'auth0-js';
+
+class Auth { 
+    constructor() { 
+        this.auth0 = new auth0.WebAuth({ 
+            domain: 'dev-u2yae36t.auth0.com',
+            audience: 'https://dev-u2yae36t.auth0.com/userinfo',
+            clientID: 'EgHE0ybb9heFp60stroxdcwRNgGqiNxC',
+            redirectUri: 'http://localhost:3000/callback',
+            scope: 'openid profile'
+
+        });
+
+        this.getProfile = this.getProfile.bind(this);
+        this.handleAuthentication = this.handleAuthentication.bind(this);
+        this.isAuthenticated = this.isAuthenticated.bind(this);
+        this.signIn = this.signIn.bind(this);
+        this.signOut = this.signOut.bind(this);
+    }
+
+    getProfile() {
+        return this.profile;
+    }
+
+    getIdToken() { 
+        return this.idToken;
+    }
+
+    isAuthenticated() { 
+        return new Date().getTime() < this.expiresAt;
+    }
+
+    signIn() { 
+        this.auth0.authorize();
+    }
+
+    handleAuthentication() { 
+        return new Promise((resolve, reject) => { 
+            this.auth0.parseHash((err, authResult) => { 
+                if(err) return reject(err); 
+                if (!authResult || !authResult.idToken) { 
+                    return reject(err);
+                }
+                this.idToken = authResult.idToken; 
+                this.profile = authResult.idTokenPayload; 
+                // set the time that the id token will expire 
+                this.expiresAt = authResult.idTokenPayload.exp * 1000;
+                resolve();
+            });
+        })
+    }
+
+    signOut() { 
+        // clear id token, profile, and expiration
+        this.idToken = null; 
+        this.profile = null; 
+        this.expiresAt = null;
+    }
+}
+
+const Auth0Client = new Auth();
+
+export default Auth0Client; 
