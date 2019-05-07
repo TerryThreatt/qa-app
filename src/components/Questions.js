c45869cfe4d9e61a0a34c810d155a82cca2dd6f9
Terry Threatt <terry.threatt@gmail.com>
2019-05-06 20:49:20 -0500

    updated spacing

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -25,6 +25,10 @@ class Questions extends Component {
           <Link to="/new-question">
             <div className="card text-white bg-secondary mb-3">
               <div className="card-header">Need help? Ask here!</div>
+              <div className="card-body">
+                <h4 className="card-title">+ New Question</h4>
+                <p className="card-text">Don't worry. Help is on the way!</p>
+              </div>
             </div>
           </Link>
           {this.state.questions === null && <p>Loading questions...</p>}
